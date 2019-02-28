a93bd92787108cf656247a4019eded9169a7abe3
Rith <menchon12622@iesmarenostrum.com>
2019-02-28 16:42:54 +0100

    Boton añadido

--- ProgramaSi/ProgramaSi/ClaseAlumnoyAlumnos.cs
+++ ProgramaSi/ProgramaSi/ClaseAlumnoyAlumnos.cs
@@ -14,6 +14,7 @@ namespace WindowsFormsApp1
 
     public partial class Form1 : Form
     {
+        private Button button1;
         Alumnos misAlumnos = new Alumnos();
 
         public Form1()
@@ -35,11 +36,22 @@ namespace WindowsFormsApp1
 
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(61, 51);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 82);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
