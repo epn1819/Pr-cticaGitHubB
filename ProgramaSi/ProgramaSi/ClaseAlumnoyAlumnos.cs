858195893bafc4421b2bc5e9cf25597d96f8d813
Pomares19728 <Pomares19728@AULA0925>
2019-03-07 16:41:36 +0100

    Machista

--- ProgramaSi/ProgramaSi/ClaseAlumnoyAlumnos.cs
+++ ProgramaSi/ProgramaSi/ClaseAlumnoyAlumnos.cs
@@ -86,9 +86,9 @@ namespace WindowsFormsApp1
             // 
             this.button1.Location = new System.Drawing.Point(402, 58);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(142, 23);
+            this.button1.Size = new System.Drawing.Size(216, 23);
             this.button1.TabIndex = 8;
-            this.button1.Text = "Guardar Alumno";
+            this.button1.Text = "Guardar Alumno/a (machista)";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click_2);
             // 
